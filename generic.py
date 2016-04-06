ee1021204c6a928ee692f99f58b61f79af7c6d4f
Steph <stephanie@ebookingservices.com>
2016-04-06 16:41:56 +0200

    new

---
+++ generic.py
@@ -0,0 +1,106 @@
+#BookYogaRetreats
+#Generic Keywords
+
+######################################################################################
+
+import sys
+
+#set max cpc bid for keyword and ad group here
+print "Please type in the max cpc for the landing page."
+max_cpc = float(raw_input())
+
+if type(max_cpc) != float:
+    sys.exit("Please type in a price in form of 0.00")
+
+######################################################################################
+
+yoga = ['yoga']
+
+holiday_word1 = [
+'holiday',
+'holidays',
+'retreat',
+'retreats',
+'vacation',
+'vacations',
+'resort',
+'resorts',
+'camp',
+'camps',
+'package',
+'packages',
+'center',
+'centers',
+'centre',
+'centres',
+'deal',
+'deals',
+'training',
+'trainings'
+]
+
+holiday_word2 = [
+'holiday',
+'holidays',
+'retreat',
+'retreats',
+'vacation',
+'vacations',
+'resort',
+'resorts',
+'camp',
+'camps',
+'package',
+'packages',
+'center',
+'centers',
+'centre',
+'centres',
+'deal',
+'deals',
+'training',
+'trainings',
+'']
+
+holiday_word3 = [
+'holiday',
+'holidays',
+'retreat',
+'retreats',
+'vacation',
+'vacations',
+'resort',
+'resorts',
+'camp',
+'camps',
+'package',
+'packages',
+'center',
+'centers',
+'centre',
+'centres',
+'deal',
+'deals',
+'training',
+'trainings',
+'']
+
+months = [
+'January',
+'February',
+'March',
+'April',
+'May',
+'June',
+'July',
+'August',
+'September',
+'October',
+'Novebmer',
+'December',
+'']
+
+year = ['2016', '']
+
+
+a1 = [yoga, holiday_word1, holiday_word2, holiday_word3, months, year] #ex) yoga retreat holiday package
