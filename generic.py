bd45bb8031222fe1c834d61e6bf79772239f3a82
Steph <stephanie@ebookingservices.com>
2016-04-07 14:02:58 +0200

    update

--- generic.py
+++ generic.py
@@ -16,7 +16,7 @@ if type(max_cpc) != float:
 
 yoga = ['yoga']
 
-holiday_words = [
+holiday_word1 = [
 'holiday',
 'holidays',
 'retreat',
@@ -279,6 +279,17 @@ holiday_words = [
 'trainings centres'
 ]
 
+holiday_word2 = [
+'package',
+'packages',
+'resort',
+'resorts',
+'retreat',
+'retreats',
+'deal',
+'deals',
+'']
+
 months = [
 'January',
 'February',
@@ -296,5 +307,57 @@ months = [
 
 year = ['2016', '']
 
+######################################################################################
 
-a1 = [yoga, holiday_word1, holiday_word2, holiday_word3, months, year] #ex) yoga retreat holiday package
+#structure and combination
+a1 = [yoga, holiday_word1, holiday_word2] #months, year] #ex) yoga retreat holiday package
+
+import itertools
+comb = list(itertools.product(*a1))
+
+######################################################################################
+
+#clean the list
+for i in range(len(comb)): #remove all empty strings
+    comb[i] = tuple(x for x in comb[i] if x != '')
+    comb[i] = ' '.join(comb[i])
+    comb[i] = tuple(comb[i].split())
+
+all_comb = comb
+
+for i in range(len(all_comb)): #remove adjacent duplicates
+    a = all_comb[i]
+    for x in range(len(a)-1):
+        if (a[x] == a[x+1]) or (a[x]+'s' == a[x+1]) or (a[x] == a[x+1]+'s'):
+            all_comb[i] = a[:x+1] + a[x+2:]
+
+all_comb = sorted(set(all_comb)) #remove duplicates and sort the list in an alphabetical order 
+
+def remove_duplicates(values): #aux
+    output = []
+    seen = set()
+    for value in values:
+        # If value has not been encountered yet,
+        # ... add it to both list and set.
+        if value not in seen:
+            output.append(value)
+            seen.add(value)
+    return output
+
+for i in range(len(all_comb)): #remove all duplicate words 
+    all_comb[i] = tuple(remove_duplicates(all_comb[i]))
+    all_comb[i] = '[' + ' '.join(all_comb[i]) + ']'
+
+######################################################################################
+
+#export keywords onto a csv file
+import csv
+
+file_name = "%s Generic.csv" %all_comb[0]
+
+with open(file_name, 'wb') as f:
+    writer = csv.writer(f)
+    for i in range(len(all_comb)):
+        writer.writerow([all_comb[i]])
+
+print "[Notification] %s generic keywords are generated successfully. Please check your current directory for the output in a CSV file." %len(all_comb)
