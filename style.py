caeedb10289e5427bd29c46bf1f9c64863167b69
Steph <stephanie@ebookingservices.com>
2016-04-13 13:37:58 +0200

    new

---
+++ style.py
@@ -0,0 +1,21 @@
+#BookYogaRetreats
+#Style Only Keywords
+
+######################################################################################
+
+import sys
+
+#user provides the style 
+
+print "Please type in the style of yoga."
+style = raw_input() + 'yoga'
+
+#set max cpc bid for keyword and ad group here
+print "Please type in the max cpc for the landing page."
+max_cpc = float(raw_input())
+
+if type(max_cpc) != float:
+    sys.exit("Please type in a price in form of 0.00")
+
+######################################################################################
+
